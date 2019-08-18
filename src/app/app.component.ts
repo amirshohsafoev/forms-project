afeb9dbc25a51faf7c601b6bb091deb823cb1e4f
amirshohsafoev <amirsafoev0@gmail.com>
2019-08-18 17:01:49 -0400

    TD control form, submitting the form, resetting and etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms-project';
+
+  @ViewChild('f', {static:false}) signUpForm: NgForm
+  defaultQuestion = 'teacher'
+  answer = ''
+  genders = ['male', 'female']
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+  submittedForm = false
+
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // this.signUpForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
+    this.signUpForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
+  }
+//Submitting the form by creating alias and 'ngForm'
+  // onSubmit(form: NgForm){
+  //   console.log(form);
+    
+  // }
+//Alternative approach for submitting the form by using @viewChild
+  onSubmit(){
+    this.submittedForm = true
+this.user.username = this.signUpForm.value.userData.username
+this.user.email = this.signUpForm.value.userData.email
+this.user.secretQuestion = this.signUpForm.value.secret
+this.user.answer = this.signUpForm.value.questionAnswer
+this.user.gender = this.signUpForm.value.gender
+
+this.signUpForm.reset()
+  }
 }
